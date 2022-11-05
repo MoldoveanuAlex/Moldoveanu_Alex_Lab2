bd68837e6bec7584e80fcac365873fb6af3b9ea7
MoldoveanuAlex <115549146+MoldoveanuAlex@users.noreply.github.com>
2022-11-05 13:19:38 +0200

    PublisherIndexData added + Publishers page functionalities

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Moldoveanu_Alex_Laborator2.Models;
+
+namespace Moldoveanu_Alex_Laborator2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Moldoveanu_Alex_Laborator2.Data;
 using Moldoveanu_Alex_Laborator2.Models;
+using Moldoveanu_Alex_Laborator2.Models.ViewModels;
 
 namespace Moldoveanu_Alex_Laborator2.Pages.Publishers
 {
@@ -21,11 +22,36 @@ namespace Moldoveanu_Alex_Laborator2.Pages.Publishers
 
         public IList<Publisher> Publisher { get;set; } = default!;
 
+
+        /*
         public async Task OnGetAsync()
         {
             if (_context.Publisher != null)
             {
                 Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+        */
+
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
+        {
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+                .Include(i => i.Books)
+                    .ThenInclude(c => c.Author)
+                .OrderBy(i => i.PublisherName)
+                .ToListAsync();
+
+            if(id != null)
+            {
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                    .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
             }
         }
     }
